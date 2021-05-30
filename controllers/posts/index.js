431a6bd09254b1ae850866732f832eb079cf7230
chj93 <chj942@gmail.com>
2021-05-30 18:55:34 +0900

    final

--- controllers/posts/index.js
+++ controllers/posts/index.js
@@ -3,7 +3,7 @@ const router = Router();
 const ctrl = require('./posts.ctrl')
 const paginate = require('express-paginate');
 
-router.get("/", paginate.middleware(2) , ctrl.get_posts_list );
+router.get("/", paginate.middleware(5) , ctrl.get_posts_list );
 router.post("/", ctrl.post_posts_write);
 router.put("/:id", ctrl.put_posts_edit);
 router.delete("/:id", ctrl.delete_posts);
